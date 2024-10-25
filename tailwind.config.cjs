b43a39fd292e561cfb69d7bea20868e956e14050
JBowen99 <josephwbowen99@gmail.com>
2024-10-25 09:37:54 -0600

    Added theme switcher and corrected theme toggle

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,24 +19,15 @@ module.exports = {
     },
   },
   daisyui: {
-    themes: [
-      "light",
-      "dark",
-      "cupcake",
-      "cyberpunk",
-      "cmyk",
-      "luxury",
-      "lemonade",
-      "night",
-      "coffee",
-      "dim",
-      "pastel",
-      "synthwave",
-      "sunset",
-      "bumblebee",
-      "forest",
-      "retro",
-    ],
+    themes: true, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
   },
   plugins: [require("daisyui")],
+  darkMode: ["class"],
 };
