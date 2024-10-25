976fc7bd88acbb75a4955ebb936e659d99ddf50a
JBowen99 <josephwbowen99@gmail.com>
2024-10-25 11:37:05 -0600

    Updated to fix google lighthouse metrics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
 
 import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +17,19 @@ export default defineConfig({
         tabler: ["*"],
       },
     }),
+    compress({
+      CSS: true,
+      HTML: {
+        "html-minifier-terser": {
+          removeAttributeQuotes: false,
+        },
+      },
+      Image: true,
+      JavaScript: true,
+      SVG: true,
+      Logger: 1,
+    }),
+    sitemap(),
   ],
+  site: "https://github.com/JBowen99/daisyShip",
 });
